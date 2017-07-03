39828462c5e495d5ba6c2a6cf86819035b5505b0
Bob Dunn <rpdunn@gmail.com>
2017-07-03 13:58:39 -0600

    add test project (does not build)

---
+++ Dunnject.Tests/ContainerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dunnject.Tests
+{
+    public class When_container_is_initialized
+    {
+        Container container;
+
+        public When_container_is_initialized()
+        {
+            container = new Container();
+        }
+    }
+}
