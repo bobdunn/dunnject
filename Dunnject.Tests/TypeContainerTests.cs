e49a555ddb5895e98764ab13854ba99c24977116
Bob Dunn <rpdunn@gmail.com>
2017-07-04 15:39:25 -0600

    add ability to enumerate dependencies to TypeContainer

--- Dunnject.Tests/TypeContainerTests.cs
+++ Dunnject.Tests/TypeContainerTests.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace Dunnject.Tests
 {
-    class TypeContainerTests
+    public class When_using_a_TypeContainer
     {
+        [Fact]
+        public void it_should_expose_required_dependencies()
+        {
+            var typeContainer = new TypeContainer(typeof(DependentClass), typeof(DependentClass));
+            Assert.Contains(typeof(IDependency), typeContainer.GetDependencies());
+        }
+
     }
+
+    public class DependentClass
+    {
+        public DependentClass(IDependency dependency)
+        {
+
+        }
+    }
+    public interface IDependency { }
 }
---
+++ Dunnject/TypeContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace Dunnject
+{
+    public class TypeContainer
+    {
+        private Type abstractType;
+        private Type concreteType;
+
+        public TypeContainer(Type abstractType, Type concreteType)
+        {
+            this.abstractType = abstractType;
+            this.concreteType = concreteType;
+        }
+
+        public IEnumerable<Type> GetDependencies()
+        {
+            var constructor = concreteType.GetTypeInfo().DeclaredConstructors.OrderBy(x=>x.GetParameters().Length).First();
+            return constructor.GetParameters().Select(x => x.ParameterType);
+        }
+    }
+}
